731b51cae309275e46ade15e3622656f0725c77a
Omar Iraqy <omariraqy16@gmail.com>
2024-05-09 20:51:16 +0300

    Reliable data transfer done.

--- receiver.py
+++ receiver.py
@@ -1,24 +1,111 @@
 import socket
+from threading import Thread, Lock
+from packet import Packet
 
-def receive_message(server_address, server_port):
-    # Create a UDP socket
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
-        # Bind the socket to the server address and port
-        sock.bind((server_address, server_port))
-        print(f"Listening on {server_address}:{server_port}")
-        
-        # Receive messages continuously
-        while True:
-            # Receive data from the socket
-            data, addr = sock.recvfrom(1024)  # Buffer size is 1024 bytes
-            print(f"Received: {data.decode()} from {addr}")
-            
-            # Send Message back to Sender
-            message = "Received Thanks"
-            sock.sendto(message.encode(), addr) 
+HOST = '127.0.0.1'  
+MAX_PACKET_SIZE = 33000
+
+add_packet_lock = Lock()
+find_defragment_lock = Lock()
 
 
-receiver_ip = "localhost" 
-receiver_port = 12345
-# Receive messages on the server
-receive_message(receiver_ip, receiver_port)
+class TCPRecvDefragment:
+    def __init__(self, sender_addr, pid):
+        self.sender_addr = sender_addr
+        self.pid = pid
+        self.packet_list = []
+
+    def add_packet(self, packet):
+        if Packet.checksum(packet) != packet.checksum:
+            return False
+
+        if packet.get_type() == 'FIN' and packet.seq > len(self.packet_list):
+            return False
+
+        with add_packet_lock:
+            if packet in self.packet_list:
+                return True
+
+            self.packet_list.append(packet)
+
+        return True
+
+    def __eq__(self, other):
+        return self.sender_addr == other.sender_addr and self.pid == other.pid
+
+    def write_out(self):
+        filename = self.sender_addr[0].replace(
+            '.', '_') + '_' + str(self.sender_addr[1]) + '_' + str(self.pid) + '.out'
+
+        with open(filename, 'wb+') as f:
+            self.packet_list.sort()
+            for packet in self.packet_list:
+                f.write(packet.data)
+
+        return filename
+
+
+class TCPRecvThread(Thread):
+
+    def __init__(self, sock, sender_addr, data, all_tcp_recv):
+        Thread.__init__(self)
+        self.sock = sock
+        self.sender_addr = sender_addr
+        self.data = data
+        self.all_tcp_recv = all_tcp_recv
+
+    def run(self):
+        recv_packet = Packet.from_bytes(self.data)
+        print(f'{self.sender_addr} -> {str(recv_packet)}')
+
+        packet_id = recv_packet.id
+        packet_seq = recv_packet.seq
+        curr_tcp_recv = TCPRecvDefragment(self.sender_addr, packet_id)
+        reply = None
+
+        with find_defragment_lock:
+            for instance in self.all_tcp_recv:
+                if instance == curr_tcp_recv:
+                    curr_tcp_recv = instance
+                    break
+            else:
+                self.all_tcp_recv.append(curr_tcp_recv)
+
+        if (curr_tcp_recv.add_packet(recv_packet)):
+            reply = recv_packet.get_reply()
+
+        if reply is not None:
+            self.sock.sendto(reply.to_bytes(), self.sender_addr)
+            print(f'{self.sender_addr} <- {str(reply)}')
+
+            if reply.get_type() == 'FIN-ACK':
+                filename = curr_tcp_recv.write_out()
+
+                if curr_tcp_recv in self.all_tcp_recv:
+                    self.all_tcp_recv.remove(curr_tcp_recv)
+                print(
+                    f'[i] Written file from {self.sender_addr} with id {packet_id} to {filename}')
+        else:
+            print(f'DROP {str(recv_packet)}')
+
+
+class TCPRecv:
+
+    def __init__(self, addr):
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+            s.bind(addr)
+            all_tcp_recv = []
+            print(f'[i] Listening on {addr}')
+
+            while True:
+                data, addr = s.recvfrom(MAX_PACKET_SIZE)
+
+                conn_thread = TCPRecvThread(s, addr, data, all_tcp_recv)
+                conn_thread.start()
+
+
+if __name__ == "__main__":
+    addr_port = int(input('Enter port to bind: '))
+    addr = (HOST, addr_port)
+
+    TCPRecv(addr)
--- sender.py
+++ sender.py
@@ -1,24 +1,131 @@
 import socket
+from threading import Thread, Event, Lock
+from packet import Packet
+from itertools import chain, islice
+from random import randint
 
-def send_message(message, receiver_ip, receiver_port):
-    # Create a UDP socket
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
-        # Send the message
-        sock.sendto(message.encode(), (receiver_ip, receiver_port))
-        print(f"Sent: {message}")
-        while True:
-            # Receive data from the socket
-            data, addr = sock.recvfrom(1024)  # Buffer size is 1024 bytes
-            print(f"Received: {data.decode()} from {addr}")
+HOST = '127.0.0.1'
 
+MAX_DATA_SIZE = 32768
+MAX_PACKET_SIZE = 33000
+MAX_SINGLE_SEND = 5
 
 
+class TCPSendThread(Thread):
+
+    def __init__(self, src, dest, timeout, packets, event):
+        Thread.__init__(self)
+        self.stopped = event
+        self.src = src
+        self.dest = dest
+        self.timeout = timeout
+        self.unacknowledged_packets = packets
+        self.pid = self.unacknowledged_packets[0].id
+
+    def run(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+            sock.bind(self.src)
+
+            ack_thread = TCPAckThread(
+                self.pid, self.unacknowledged_packets, sock)
+            ack_thread.start()
+
+            for unacknowledged_packet in self.unacknowledged_packets[:MAX_SINGLE_SEND]:
+                sock.sendto(unacknowledged_packet.to_bytes(), self.dest)
+                print(f'{self.dest} <- {unacknowledged_packet}')
+
+            while not self.stopped.wait(self.timeout):
+                if len(self.unacknowledged_packets) == 0:
+                    self.stopped.set()
+                for unacknowledged_packet in self.unacknowledged_packets[:MAX_SINGLE_SEND]:
+                    sock.sendto(
+                        unacknowledged_packet.to_bytes(), self.dest)
+                    print(f'{self.dest} <- {unacknowledged_packet}')
+
+            ack_thread.join()
+
+        print(f'[i] All package for id {self.pid} sent!')
 
 
-# Sender settings
-receiver_ip = "localhost"  # Using localhost
-receiver_port = 12345
-# Example message to send
-message_to_send = "Hello, UDP Server!"
-# Send message to the server
-send_message(message_to_send, receiver_ip, receiver_port)
+class TCPAckThread(Thread):
+
+    def __init__(self, pid, unacknowledged_packets, sock):
+        Thread.__init__(self)
+        self.stopped = Event()
+        self.unacknowledged_packets = unacknowledged_packets
+        self.sock = sock
+        self.pid = pid
+
+    def run(self):
+        while not self.stopped.is_set():
+            data, addr = self.sock.recvfrom(MAX_PACKET_SIZE)
+            ack_packet = Packet.from_bytes(data)
+            print(f'{addr} -> {ack_packet}')
+
+            if ack_packet in self.unacknowledged_packets:
+                self.unacknowledged_packets.remove(ack_packet)
+
+            if (ack_packet.get_type() == 'FIN-ACK'):
+                self.stopped.set()
+
+        print(f'[i] All package for id {self.pid} acknowledged!')
+
+
+class TCPSend:
+
+    def __init__(self, dest, timeout, files):
+        all_packets = []
+
+        for file_to_split in files:
+            all_packets.append(list(self.file_to_packets(file_to_split)))
+
+        stop_flags = []
+        send_threads = []
+        base_port = randint(1025, 65534)
+
+        for idx, single_file_packets in enumerate(all_packets):
+            stop_flag = Event()
+            stop_flags.append(stop_flag)
+
+            src = (HOST, base_port + idx)
+
+            send_thread = TCPSendThread(
+                src, dest, timeout, single_file_packets, stop_flag)
+            send_threads.append(send_thread)
+            send_thread.start()
+
+        for idx, thread in enumerate(send_threads):
+            thread.join()
+
+    @staticmethod
+    def file_to_packets(filename):
+        try:
+            with open(filename, 'rb') as file_to_split:
+                pid = Packet.pick_id()
+                seq = 0
+                chunk = file_to_split.read(MAX_DATA_SIZE)
+
+                while chunk:
+                    next_chunk = file_to_split.read(MAX_DATA_SIZE)
+                    if next_chunk:
+                        yield Packet('DATA', pid, seq, len(chunk), chunk)
+                        chunk = next_chunk
+                    else:
+                        yield Packet('FIN', pid, seq, len(chunk), chunk)
+                        chunk = False
+                    seq += 1
+        except OSError as err:
+            print(f'File {filename} doesn\'t exist')
+
+
+if __name__ == '__main__':
+    dest_ip = HOST
+    dest_port = input('Enter destination (Port): ')
+    dest = (dest_ip.strip(), int(dest_port.strip()))
+
+    timeout = float(input('Timeout (s): '))
+
+    files = input('Files to send (Separated by comma): ')
+    files = [f.strip() for f in files.split(',')]
+
+    TCPSend(dest, timeout, files)
